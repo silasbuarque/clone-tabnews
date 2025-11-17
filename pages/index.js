8431a11749c40ec3b3ac22a285daf73adaefc490
Silas Buarque <60887654+silasbuarque@users.noreply.github.com>
2025-11-17 00:57:01 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,5 @@
-import { useEffect } from "react";
-
 function Home() {
-
-    useEffect(() => {
-        document.body.style.margin = "0";
-        document.body.style.padding = "0";
-    }, []);
-
-    return (
-        <div
-            style={{
-                height: "100vh",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundColor: "#0F0005",
-            }}
-        >
-            <h1
-                style={{
-                    color: "#E59BB1",
-                    textShadow: "2px 2px 4px rgba(0,0,0,0.25)",
-                    textAlign: "center",
-                    fontSize: "2rem"
-                }}
-            >
-                Ariany, eu amo você. Se você me ama, dá uma risadinha!
-            </h1>
-        </div>
-    );
+  return <h1>Ariany, eu amo você. Se você me ama, dá uma risadinha!</h1>;
 }
 
 export default Home;
